69df5d9cb363cb44f4530fbd830d12f895ee0ab7
quiland15 <quiland86@gmail.com>
2025-04-29 18:59:53 +0800

    make database integration to vps databases

--- latihan/latihan/settings.py
+++ latihan/latihan/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'latihan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_DevOps',
+        'USER': 'Quiland',
+        'PASSWORD': 'Quiland15september_',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
